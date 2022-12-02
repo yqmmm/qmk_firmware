07766d453ddbb254d49b371c21e322790cc17030
Qianmian Yu <im.qianmian.yu@gmail.com>
2022-12-02 12:32:55 +0800

    [Q8] Use right space as GUI key.

--- keyboards/keychron/q8/q8_ansi_stm32l432_ec11/keymaps/yqmmm/keymap.c
+++ keyboards/keychron/q8/q8_ansi_stm32l432_ec11/keymaps/yqmmm/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,          KC_DEL,
         KC_LCTL, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,           KC_HOME,
         KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LWIN,  KC_LALT,     LT(VIM, KC_SPC),        MO(_FN2), MO(_FN3),         KC_SPC,            KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LWIN,  KC_LALT,     LT(VIM, KC_SPC),        MO(_FN2), MO(_FN3),     RGUI_T(KC_SPC),        KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
 
     [_FN1] = LAYOUT_ansi_69(
         KC_GRV,  KC_BRID,  KC_BRIU,  KC_MCTL, KC_LPAD, RGB_VAD, RGB_VAI,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,          _______,
@@ -86,8 +86,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, _______, _______),
     [VIM] = LAYOUT_ansi_69(
         KC_TILD, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,        KC_F9,     KC_F10,   KC_F11,   KC_F12,   _______,          _______,
-        RGB_TOG, RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI, _______,  _______, C(S(KC_TAB)), C(KC_TAB), _______,  _______,  _______,  _______,          _______,
-        _______, RGB_RMOD, RGB_VAD,  RGB_HUD, RGB_SAD, RGB_SPD,           KC_LEFT, KC_DOWN,      KC_UP,     KC_RGHT,  _______,  _______,  _______,          _______,
+        RGB_TOG, RGB_MOD,  RGB_VAI,  KC_END, RGB_SAI, RGB_SPI, _______,  _______, C(S(KC_TAB)), C(KC_TAB), _______,  _______,  _______,  _______,          _______,
+        _______, KC_HOME,  RGB_VAD,  RGB_HUD, RGB_SAD, RGB_SPD,           KC_LEFT, KC_DOWN,      KC_UP,     KC_RGHT,  _______,  _______,  _______,          _______,
         _______,           _______,  _______, _______, _______, _______,  _______, _______,      _______,   _______,  _______,  _______,  _______, _______,
         _______, _______,  _______,           _______,          _______,  _______,               _______,             _______,            _______, _______, _______)
 };
